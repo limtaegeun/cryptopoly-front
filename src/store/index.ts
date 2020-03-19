8721cd04c0aaad1159f8533a19c3f013ff83ae62
taegeun lim <imori333@gmail.com>
2020-03-20 00:23:05 +0900

    fix store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,17 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+// Store functionality
+import actions from "./actions";
+import getters from "./getters";
+import mutations from "./mutations";
+import state from "./state";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  actions,
+  getters,
+  mutations,
+  state
 });
