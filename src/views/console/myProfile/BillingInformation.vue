570c7a049e9de84a6251284288229a6bc581348f
taegeun lim <imori333@gmail.com>
2020-07-28 16:09:58 +0900

    add billing information inside

--- src/views/console/myProfile/BillingInformation.vue
+++ src/views/console/myProfile/BillingInformation.vue
@@ -1,4 +1,50 @@
-<template> <div></div></template>
+<template>
+  <div>
+    <h1>Billing Information</h1>
+    <div class="info-wrapper">
+      <v-row>
+        <v-col cols="12">
+          <v-text-field
+            class="info-text-field"
+            outlined
+            dense
+            label="Regular"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12">
+          <v-text-field
+            class="info-text-field" dense
+            outlined
+            label="Regular"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="4">
+          <v-text-field class="info-text-field" dense
+            outlined
+            label="Regular"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="4">
+          <v-text-field class="info-text-field" dense
+            outlined
+            label="Regular"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="4">
+          <v-text-field class="info-text-field" dense
+            outlined
+            label="Regular"
+          ></v-text-field>
+        </v-col>
+
+      </v-row>
+      <v-row class="button-wrapper">
+        <v-btn style="margin-right: 12px">Edit</v-btn>
+        <v-btn>Delete</v-btn>
+      </v-row>
+    </div>
+  </div>
+</template>
 
 <script lang="ts">
 import Vue from "vue";
@@ -11,4 +57,14 @@ export default Vue.extend({
 });
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+  .info-wrapper {
+    max-width: 500px;
+  }
+  .button-wrapper {
+    padding: 12px;
+  }
+  .info-text-field {
+    height: 25px;
+  }
+</style>
